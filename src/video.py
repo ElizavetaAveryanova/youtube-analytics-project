40457e1d6d92636625f13ce8650536816112e47e
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-01-31 15:30:53 +0500

    homework_6

--- src/video.py
+++ src/video.py
@@ -10,14 +10,25 @@ class Video:
         """Экземпляр инициализируется по id видео """
         self.video_id: str = video_id  # id видео
 
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=self.video_id).execute()
 
-        # Создаём необходимые атрибуты
-        self.title: str = self.video_response['items'][0]['snippet']['title'] # название видео
-        self.url: str = f'https://www.youtube.com/watch?v={self.video_id}' # ссылка на видео
-        self.viewCount: int = self.video_response['items'][0]['statistics']['viewCount'] # количество просмотров
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount'] # количество лайков
+    # Пробуем получить данные видео по api
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                    id=self.video_id).execute()
+            video_data_is_empty = self.video_response['items'][0]
+        # Если получить данные о видео невозможно (несуществующий id), все атрибуты приравниваем к None
+        except IndexError:
+            self.title = None  # название видео
+            self.url = None  # ссылка на видео
+            self.viewCount = None  # количество просмотров
+            self.like_count = None  # количество лайков
+
+        # Если исключения не возникло и данные получены, создаём необходимые атрибуты
+        else:
+            self.title: str = self.video_response['items'][0]['snippet']['title'] # название видео
+            self.url: str = f"https://www.youtube.com/watch?v={self.video_response['items'][0]['id']}"  # ссылка на видео
+            self.viewCount: int = self.video_response['items'][0]['statistics']['viewCount'] # количество просмотров
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount'] # количество лайков
 
     def __str__(self) -> str:
         """Возвращает название видео """
